b4e27f3632bd830c5f0f3b1899a1c97b165ca963
Nikita Vasyukov <n.vasyukov@corp.vk.com>
2022-04-08 06:01:09 +0300

    Animation and modal improvements

--- src/modals/PhotoPostModal/PhotoPostModal.tsx
+++ src/modals/PhotoPostModal/PhotoPostModal.tsx
@@ -1,54 +1,92 @@
-import { FC, memo } from "react";
+import { FC, memo, useState } from "react";
 import { TDefaultModal } from "../TDefaultModal";
-import { Div, ModalPage, Text } from "@vkontakte/vkui";
+import { Div, ModalCard, Text } from "@vkontakte/vkui";
 import { useDispatch, useSelector } from "react-redux";
 import { selectActivePhotoPost } from "../../store/lenta/selectors/selectActivePhotoPost";
 import "./PhotoPostModal.css";
-import { Icon28Like, Icon28LikeOutline } from "@vkontakte/icons";
+import {
+  Icon28Like,
+  Icon28LikeOutline,
+  Icon36LikeOutline,
+} from "@vkontakte/icons";
 import { removeLike } from "../../store/lenta/sets/removeLike";
 import { addLike } from "../../store/lenta/sets/addLike";
 import { DateTime } from "luxon";
+import bridge from "@vkontakte/vk-bridge";
 
 export const PhotoPostModal: FC<TDefaultModal> = memo(({ id, onClose }) => {
   const activePhotoPost = useSelector(selectActivePhotoPost);
   const dispatch = useDispatch();
+  const [animatedClasses, setAnimatedClasses] = useState("");
+  const [bigAnimatedClasses, setBigAnimatedClasses] = useState("");
+  const showBigAnimation = () => {
+    setBigAnimatedClasses("PhotoPostModal_bigLike--animated");
+
+    setTimeout(() => {
+      setBigAnimatedClasses("");
+    }, 300);
+  };
+  const showAnimation = () => {
+    setAnimatedClasses("PhotoPostModal_like--animated");
+
+    setTimeout(() => {
+      setAnimatedClasses("");
+    }, 500);
+  };
+
+  const triggerHaptic = () => {
+    void bridge.send("VKWebAppTapticNotificationOccurred", { type: "success" });
+  };
+
   const onClickRemoveLike = () => {
     if (activePhotoPost) {
       dispatch(removeLike(activePhotoPost.id));
     }
   };
   const onClickAddLike = () => {
-    if (activePhotoPost) {
+    if (activePhotoPost && !activePhotoPost.isLiked) {
       dispatch(addLike(activePhotoPost.id));
     }
   };
 
   return (
-    <ModalPage
-      id={id}
-      onClose={onClose}
-      settlingHeight={90}
-      dynamicContentHeight
-    >
+    <ModalCard id={id} onClose={onClose}>
       {activePhotoPost && (
         <>
           <Div className="PhotoPostModal_box">
-            <img
-              className="PhotoPostModal_img"
-              src={activePhotoPost.highSrc}
-              alt=""
-            />
+            <div className="PhotoPostModal_imgBox">
+              <img
+                className="PhotoPostModal_img"
+                onDoubleClick={() => {
+                  triggerHaptic();
+                  onClickAddLike();
+                  showBigAnimation();
+                }}
+                src={activePhotoPost.highSrc}
+                alt=""
+              />
+              <Icon36LikeOutline
+                className={"PhotoPostModal_bigLike " + bigAnimatedClasses}
+              />
+            </div>
           </Div>
           <Div className="PhotoPostModal_metaRow">
             {activePhotoPost.isLiked ? (
               <Icon28Like
-                className="PhotoPostModal_icon"
-                onClick={onClickRemoveLike}
+                className={"PhotoPostModal_icon " + animatedClasses}
+                onClick={() => {
+                  showAnimation();
+                  onClickRemoveLike();
+                }}
               />
             ) : (
               <Icon28LikeOutline
-                className="PhotoPostModal_icon"
-                onClick={onClickAddLike}
+                className={"PhotoPostModal_icon " + animatedClasses}
+                onClick={() => {
+                  showAnimation();
+                  onClickAddLike();
+                  triggerHaptic();
+                }}
               />
             )}
             <Text className="PhotoPostModal_likesCount" weight="semibold">
@@ -62,6 +100,6 @@ export const PhotoPostModal: FC<TDefaultModal> = memo(({ id, onClose }) => {
           </Div>
         </>
       )}
-    </ModalPage>
+    </ModalCard>
   );
 });
--- src/panels/Profile/Profile.tsx
+++ src/panels/Profile/Profile.tsx
@@ -1,19 +1,52 @@
 import { TPanel } from "../TPanel";
-import { Panel, PanelHeader } from "@vkontakte/vkui";
+import {
+  Avatar,
+  Panel,
+  PanelHeader,
+  PanelHeaderButton,
+  PanelHeaderContent,
+} from "@vkontakte/vkui";
 import { FC, memo, useMemo } from "react";
 import { ExpandedHead } from "./components/ExpandedHead/ExpandedHead";
 import { MainMenu } from "./components/MainMenu/MainMenu";
 import { useSelector } from "react-redux";
 import { selectActiveLenta } from "../../store/lenta/selectors/selectActiveLenta";
 import { menuItems } from "../../entities/MainMenuButton";
+import { Icon28Menu } from "@vkontakte/icons";
+import { selectProfileState } from "../../store/lenta/selectors/selectProfileState";
 
 export const Profile: FC<TPanel> = memo(({ id }) => {
+  const { data } = useSelector(selectProfileState);
   const activeLenta = useSelector(selectActiveLenta);
   const { Lenta } = useMemo(() => menuItems[activeLenta], [activeLenta]);
+  const badge = useMemo((): "online-mobile" | "online" | undefined => {
+    if (data?.isFromMobile) {
+      return "online-mobile";
+    }
+
+    if (data?.isOnline) {
+      return "online";
+    }
+
+    return;
+  }, [data]);
 
   return (
     <Panel id={id}>
-      <PanelHeader fixed={false}>Профиль 2.0</PanelHeader>
+      <PanelHeader
+        left={
+          <PanelHeaderButton>
+            <Icon28Menu />
+          </PanelHeaderButton>
+        }
+      >
+        <PanelHeaderContent
+          status={data?.status}
+          before={<Avatar size={36} src={data?.avatarSrcLow} badge={badge} />}
+        >
+          {data?.name}
+        </PanelHeaderContent>
+      </PanelHeader>
       <ExpandedHead />
       <MainMenu />
       <Lenta />
